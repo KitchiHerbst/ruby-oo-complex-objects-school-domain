f1828de5f29835970a940a6d4a984adea71bff33
Triston <tristonherbst97@gmail.com>
2021-02-23 21:05:29 -0600

    commit tree fb19a5bb61e459925634d1a23010b86ed9314a9d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    def initialize(name, roster={})
+        @name=name
+        @roster=roster
+    end
+    def name
+        @name
+    end
+    def roster
+        @roster
+    end
+    def add_student=(student,grade)
+        @student=student
+        @grade=grade
+        @add_student=@roster[@grade].uniq = []
+        @roster[@grade] << @student
+    end
+    def student
+        @student
+    end
+    def grade
+        @grade
+    end
+end
